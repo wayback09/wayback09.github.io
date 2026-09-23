5541bbed89e51595f1658f1db30a7a0619453a85
CyberDroid456 <158224073+CyberDroid456@users.noreply.github.com>
2026-09-23 19:45:52 +0100

    feat: remove TV static overlay

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/components/footer/Footer"
 import Nav from "@/components/nav/Nav"
 import { ThemeProvider } from "@/components/ThemeProvider"
-import TvStatic from "@/components/TvStatic"
 import { DiscordEmbed } from "@/lib/discord"
 import { cn } from "@/lib/utils"
 
@@ -51,7 +50,6 @@ export default async function RootLayout({
       </head>
       <body className="relative flex min-h-screen flex-col">
         <ThemeProvider>
-          <TvStatic />
           <Nav />
           <div className="flex min-h-160 flex-1 flex-col px-8 py-16">
             {children}
